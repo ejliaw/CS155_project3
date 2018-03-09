e873fc21e62544faa4fed53340c70e0b548bc914
ruillercoaster <chengrui0208@gmail.com>
2018-03-08 20:45:28 -0800

    Rui added the codes for formatting the sonnet. Syllable meter is included. More matrix files should be added.

--- makePoems.py
+++ makePoems.py
@@ -13,8 +13,39 @@ for hmmfile in hmmfiles:
 
 
 print(len(HMMs))
-
+all_poem = []
 for HMM in HMMs:
-    xs, ys = HMM.generate_emission(120)
-    poem = [index2word[w] for w in xs]
-    print(poem)
+    xs, ys = HMM.generate_emission(400)
+    line=[]
+    poem = []
+    syll_counter = 0
+    line_counter = 0
+    for w in xs:
+        if syll_counter >= 10 and line_counter <14:
+            print('line number')
+            print(line_counter)
+            print('syllables')
+            print(syll_counter)
+            print(' '.join(line))
+            poem.append(' '.join(line))
+            line = []
+            line_counter = line_counter + 1
+            syll_counter = 0
+        else:
+            if index2word[w] != '\\':
+                line.append(index2word[w])
+                syll_counter = syll_counter + int(word2syll[index2word[w]][0][-1])        
+    print('end of sonnet')
+    all_poem.append(poem)
+print('============================================================================')
+print('============================================================================')
+for i in range(0,len(all_poem)):
+    print(hmmfiles[i][4])
+    for j in range(0,len(all_poem[i])):
+        if j == 3 or j == 7 or j==11 :
+            print(all_poem[i][j])
+            print('')
+        else:
+            print(all_poem[i][j])
+    print('========================================================================')
+    print('========================================================================')
